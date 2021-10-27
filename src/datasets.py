9f5b0be824877487d5ee21d3d46bf9edc5f7da71
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-27 06:43:37 +0100

    Write tests for datasets.py

---
+++ src/datasets.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pathlib
+noisy_dataset = np.array([])
+clean_dataset = np.array([])
---
+++ tests/test_dataset.py
@@ -0,0 +1,8 @@
+from datasets import noisy_dataset, clean_dataset
+import numpy as np
+
+def test_datasets_loaded_correctly():
+    assert noisy_dataset.shape == (2000, 8)
+    assert clean_dataset.shape == (2000, 8)
+    
+

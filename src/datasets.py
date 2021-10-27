e23f0d32bcc167d9480d4a6591017d512219e741
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-27 06:44:27 +0100

    Implement datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -1,4 +1,9 @@
 import numpy as np
 import pathlib
-noisy_dataset = np.array([])
-clean_dataset = np.array([])
+
+noisy_dataset = np.loadtxt(
+    pathlib.Path(__file__).parent / "../wifi_db/noisy_dataset.txt"
+)
+clean_dataset = np.loadtxt(
+    pathlib.Path(__file__).parent / "../wifi_db/clean_dataset.txt"
+)
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,8 +1,7 @@
 from datasets import noisy_dataset, clean_dataset
 import numpy as np
 
+
 def test_datasets_loaded_correctly():
     assert noisy_dataset.shape == (2000, 8)
     assert clean_dataset.shape == (2000, 8)
-    
-

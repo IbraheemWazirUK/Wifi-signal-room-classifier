04f1020f31b79cac32e3a482e092e37ca7bce19f
Ibraheem Wazir <ibraheem.wazir.uk@gmail.com>
2021-10-28 07:44:49 +0100

    Implement dtl to pass all tests

--- src/dtl.py
+++ src/dtl.py
@@ -1,4 +1,5 @@
 from tree import Tree
+from split import find_split
 
 
 def decision_tree_learning(training_dataset, depth):
@@ -7,4 +8,8 @@ def decision_tree_learning(training_dataset, depth):
     labels = [training_instance[-1] for training_instance in training_dataset]
     if labels and labels.count(labels[0]) == len(labels):
         return (Tree(labels[0], None, None, True), depth)
-    return (Tree(None, None, None), 0)
+    (split_rule, l_dataset, r_dataset) = find_split(training_dataset)
+    (l_branch, l_depth) = decision_tree_learning(l_dataset, depth + 1)
+    (r_branch, r_depth) = decision_tree_learning(r_dataset, depth + 1)
+    tree = Tree(split_rule, l_branch, r_branch)
+    return (tree, max(l_depth, r_depth))
